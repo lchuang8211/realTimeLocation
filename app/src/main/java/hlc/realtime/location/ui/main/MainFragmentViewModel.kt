05285814ca34c9dc38899505fe9d89ac9ba129ff
DESKTOP-OSNBSEK\Yellow <hlc>
2021-05-29 18:41:55 +0800

    [FIX] 修改 撈 LocationData 流程與邏輯

--- app/src/main/java/hlc/realtime/location/ui/main/MainFragmentViewModel.kt
+++ app/src/main/java/hlc/realtime/location/ui/main/MainFragmentViewModel.kt
@@ -1,99 +1,216 @@
 package hlc.realtime.location.ui.main
 
 import android.location.Address
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import hlc.realtime.location.base.BaseViewModel
 import hlc.realtime.location.data.api.LocationData
+import hlc.realtime.location.data.local.defaultTabble.RoomTableDemo
+import hlc.realtime.location.domain.GetLocationUseCase
 import hlc.realtime.location.support.GPSHelper
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.schedulers.Schedulers
 import timber.log.Timber
 import java.text.SimpleDateFormat
 import java.util.*
 import javax.inject.Inject
+import kotlin.collections.ArrayList
 
-class MainFragmentViewModel @Inject constructor(): BaseViewModel(){
+class MainFragmentViewModel @Inject constructor(
+    private val locationUseCase: GetLocationUseCase
+): BaseViewModel(){
 
-    var startTime = ""
-    var endTime = ""
+    private var startTime = ""
+    private var endTime = ""
 
-    val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.TAIWAN)
-    val calendarTime = Calendar.getInstance()
-    var cDay: Int = 1
-    var cYear: Int = 2021
-    var cMonth: Int = 1
-    var cWMonth: Int = 1
-    var cDWeek: Int = 0
-    var today = ""
+    private val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.TAIWAN)
 
-    val addressList = mutableListOf<Address>()
+    private var cDay : String = "01"
+    private var cYear : String = "2021"
+    private var cMonth : String = "01"
+    private var cHour : String = "01"
+    private var cMin : String = "01"
+    private var today = ""
+
+    val _oldList = MutableLiveData<MutableList<LocationData>>(arrayListOf())
+    var addressList : MutableLiveData<MutableList<LocationData>> = _oldList
+
+    private val _newList = MutableLiveData<ArrayList<LocationData>>()
+
+    private val newAddressListByGPS = mutableListOf<Address>()
 
     // record: true , release: false
     val recordStatus = MutableLiveData<Boolean>(false)
 
-    // Start : true, End : false
-    fun getCurrentTime(status: Boolean){
-        val calendarTime = Calendar.getInstance()
-        cDay = calendarTime.get(Calendar.DAY_OF_MONTH)
-        cYear = calendarTime.get(Calendar.YEAR)
-        cMonth = calendarTime.get(Calendar.MONTH) + 1
-        val hour = calendarTime.get(Calendar.HOUR_OF_DAY).toString().padStart(2, '0') // 24小時制
-        val hour2 = calendarTime.get(Calendar.HOUR) // 12小時制
-        val min = calendarTime.get(Calendar.MINUTE).toString().padStart(2, '0')
-        today = dateFormat.format(calendarTime.time)
-        Timber.tag("hlcDebug").d("getCurrentTime : $cYear / $cMonth / $cDay")
-        Timber.tag("hlcDebug").d("getCurrentTime : $hour / $hour2 / $min")
+    var totalDay : LiveData<List<LocationData.Date>> = locationUseCase.getTotalDay()
+    var todaySeq = -1
 
+    init {
+        updateTime()
+    }
+
+    fun updateTime(){
+        val calendar = Calendar.getInstance()
+        today = dateFormat.format(calendar.time)
+        cYear = (calendar.get(Calendar.YEAR)).toString().padStart(2, '0')
+        cMonth = (calendar.get(Calendar.MONTH) + 1).toString().padStart(2, '0')
+        cDay = (calendar.get(Calendar.DAY_OF_MONTH)).toString().padStart(2, '0')
+        cHour = calendar.get(Calendar.HOUR_OF_DAY).toString().padStart(2, '0') // 24小時制
+        val hour2 = calendar.get(Calendar.HOUR) // 12小時制
+        cMin = calendar.get(Calendar.MINUTE).toString().padStart(2, '0')
+
+        Timber.tag("hlcDebug").d("getCurrentTime : $cYear / $cMonth / $cDay")
+        Timber.tag("hlcDebug").d("getCurrentTime : $cHour / $hour2 / $cMin")
+    }
+
+    fun initRrcycler(year: String = cYear, month: String = cMonth, day: String = cDay, position: Int = 0){
+
+        val locationDetail = mutableListOf<LocationData.LocationDetail>()
+
+        locationUseCase(year, month, day)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                it.forEach { item->
+                    locationDetail.add(LocationData.LocationDetail(
+                        seq = item.seq,
+                        startTime = item.startTime,
+                        endTime = item.endTime,
+                        longitude = item.longitude,
+                        latitude = item.latitude,
+                        customAddress = item.customAddress,
+                        firstAddress = item.firstAddress,
+                        secondAddress = item.secondAddress,
+                        thirdAddress = item.thirdAddress
+                    ))
+                }
+                if (year == cYear && month == cMonth && day == cDay) todaySeq = it.size
+                val data = LocationData(
+                    date = LocationData.Date(year, month, day),
+                    locationDetail = locationDetail
+                )
+                _oldList.value?.set(position, data)
+                addressList.postValue(_oldList.value)
+            },{
+                Timber.tag("hlcDebug").d("locationUseCase throw : $it")
+            }).addTo(compositeDisposable)
+
+    }
+
+    // Start : true, End : false
+    fun getGPSLocationFlow(status: Boolean){
+        getCurrentTime(status)
+    }
+
+    fun getCurrentTime(status: Boolean){
+        updateTime()
         if (status) {
-            startTime = "$hour:$min"
+            startTime = "$cHour:$cMin"
             recordStatus.value = true
             getCurrentLocation(true)
         }
         else{
-            endTime = "$hour:$min"
+            endTime = "$cHour:$cMin"
             recordStatus.value = false
             getCurrentLocation(false)
+            setLocationAfterEnd()
         }
     }
-
-    private val _list = mutableListOf<LocationData>()
-    val list = MutableLiveData<List<LocationData>>()
 
     // Start : true, End : false
-    fun getCurrentLocation(status: Boolean){
-        addressList.clear()
-        addressList.addAll( GPSHelper.getCurrentAddress() )
-        if (addressList.isNotEmpty() && addressList.size > 1){
+    fun getCurrentLocation(status: Boolean) {
+
+        newAddressListByGPS.clear()
+        newAddressListByGPS.addAll( GPSHelper.getCurrentAddress() )
+
+        if (newAddressListByGPS.isNotEmpty() && newAddressListByGPS.size > 1) {
+            todaySeq+=1
 
             val detail = LocationData.LocationDetail(
+                seq = todaySeq,
                 startTime = this.startTime,
                 endTime = " ",
-                locationX = GPSHelper.getLongitude(),
-                locationY = GPSHelper.getLatitude(),
-                address = addressList[1].getAddressLine(0)
+                longitude = GPSHelper.getLongitude(),
+                latitude = GPSHelper.getLatitude(),
+                customAddress = "",
+                firstAddress = "${newAddressListByGPS[0].countryName}${newAddressListByGPS[0].adminArea}${newAddressListByGPS[0].locality}${newAddressListByGPS[0].thoroughfare}${newAddressListByGPS[0].subThoroughfare}",
+                secondAddress = "${newAddressListByGPS[1].countryName}${newAddressListByGPS[1].adminArea}${newAddressListByGPS[1].locality}${newAddressListByGPS[1].thoroughfare}${newAddressListByGPS[1].subThoroughfare}",
+                thirdAddress = "${newAddressListByGPS[2].countryName}${newAddressListByGPS[2].adminArea}${newAddressListByGPS[2].locality}${newAddressListByGPS[2].thoroughfare}${newAddressListByGPS[2].subThoroughfare}"
             )
+            // TODO 優化流程
 
             // select database
-            val find = _list.find { it.date.year == cYear.toString().padStart(2, '0') && it.date.month == cMonth.toString().padStart(2, '0') && it.date.day == cDay.toString().padStart(2, '0') }
+            _newList.value?.add(
+                LocationData(
+                    date = LocationData.Date(year = cYear, month = cMonth, day = cDay),
+                    locationDetail = mutableListOf(detail))
+            )
 
-            if (find == null){
-                _list.add(LocationData(
-                    date = LocationData.Date(
-                            year = cYear.toString().padStart(2, '0'),
-                            month = cMonth.toString().padStart(2, '0'),
-                            day = cDay.toString().padStart(2, '0')),
+            // 0 = 今天第一筆
+            if (todaySeq == 0){
+                _newList.value?.add(
+                    LocationData(
+                    date = LocationData.Date(year = cYear, month = cMonth, day = cDay),
                     locationDetail = mutableListOf(detail))
                 )
-            } else {
-                _list.filter { it.date.year == cYear.toString().padStart(2, '0') && it.date.month == cMonth.toString().padStart(2, '0') && it.date.day == cDay.toString().padStart(2, '0') }
-                    .map {
-                        if (recordStatus.value!!)
-                            it.locationDetail.add(detail)
-                        else
-                            it.locationDetail[it.locationDetail.lastIndex].endTime = this.endTime
-                    }
             }
 
-            list.value = _list
+//            val find = _oldList.find { it.date.year == cYear.toString().padStart(2, '0') && it.date.month == cMonth.toString().padStart(2, '0') && it.date.day == cDay.toString().padStart(2, '0') }
+//
+//            if (find == null){
+//                _oldList.add(LocationData(
+//                    date = LocationData.Date(
+//                            year = cYear.toString().padStart(2, '0'),
+//                            month = cMonth.toString().padStart(2, '0'),
+//                            day = cDay.toString().padStart(2, '0')),
+//                    locationDetail = mutableListOf(detail))
+//                )
+//            } else {
+//                _oldList.filter { it.date.year == cYear.toString().padStart(2, '0') && it.date.month == cMonth.toString().padStart(2, '0') && it.date.day == cDay.toString().padStart(2, '0') }
+//                    .map {
+//                        if (recordStatus.value!!)
+//                            it.locationDetail.add(detail)
+//                        else
+//                            it.locationDetail[it.locationDetail.lastIndex].endTime = this.endTime
+//                    }
+//            }
+//
+//            addressList.value = _oldList
 
         }
     }
+
+    fun setLocationAfterEnd(){
+
+        val currentLocation = RoomTableDemo(
+            year = cYear,
+            month = cMonth,
+            day = cDay,
+            seq = todaySeq,
+            startTime = startTime,
+            endTime = endTime,
+            longitude = GPSHelper.getLongitude(),
+            latitude = GPSHelper.getLatitude(),
+            customAddress = "",
+            firstAddress = newAddressListByGPS[0].getAddressLine(0),
+            secondAddress = newAddressListByGPS[1].getAddressLine(0),
+            thirdAddress = newAddressListByGPS[2].getAddressLine(0)
+        )
+
+        /** 奇怪的流程，insert data 進DB會自動回傳扁動的table (locationUseCase(year, month, day)) */
+        Single.just(1)
+            .subscribeOn(Schedulers.newThread())
+            .observeOn(Schedulers.io())
+            .subscribe({
+                // TODO 優化流程
+//              _list.clear()
+                locationUseCase.insertLocation(currentLocation)
+                Timber.tag("hlcDebug").d("insert ok: ")
+            }, {
+                Timber.tag("hlcDebug").d("insert throw: $it")
+            }).apply { compositeDisposable.add(this) }
+        }
+
 }
