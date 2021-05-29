46ed622bf624f0db7f3e0b2797d11ea2645b16fd
DESKTOP-OSNBSEK\Yellow <hlc>
2021-05-29 18:14:40 +0800

    修改 LocationData 結構

--- app/src/main/java/hlc/realtime/location/data/api/LocationData.kt
+++ app/src/main/java/hlc/realtime/location/data/api/LocationData.kt
@@ -1,25 +1,26 @@
 package hlc.realtime.location.data.api
 
-import java.time.Year
-
+import java.util.*
 
 data class LocationData(
-    val date : Date,
-    val locationDetail : MutableList<LocationDetail>
+    val date : Date ?=null ,
+    val locationDetail : MutableList<LocationDetail> ?=null
 ){
-
-    data class LocationDetail(
-        var startTime: String = "",
-        var endTime: String = "",
-        val locationX: Double,
-        val locationY: Double,
-        val address: String = ""
-    )
-
     data class Date(
         val year: String = "",
         val month: String = "",
         val day: String = ""
     )
 
+    data class LocationDetail(
+        var seq : Int = 0,
+        var startTime: String = "",
+        var endTime: String = "",
+        val longitude: Double,  // x 經度
+        val latitude: Double,  // y 緯度
+        var customAddress: String = "",
+        val firstAddress: String,
+        val secondAddress: String,
+        val thirdAddress: String
+    )
 }
